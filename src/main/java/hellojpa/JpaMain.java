61ab33ea700a4e8dc423457e2646379a7358cf92
euichanhwang <pine6971@naver.com>
2023-04-03 19:16:28 +0900

    feat: 엔티티를 준영속 상태로 만든다

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,5 @@
 package hellojpa;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -10,27 +8,17 @@ import javax.persistence.Persistence;
 public class JpaMain {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 애플리케이션 로딩 시점에 딱 하나만 만든다
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
 		EntityManager em = emf.createEntityManager();
 
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		try {
-			// Member member = new Member();
-			// member.setId(2L);
-			// member.setName("HelloB");
-			Member findMember = em.find(Member.class, 1L); // JPA가 관리. 커밋 시점에 변경 여부 체크.-> update 쿼리
-			findMember.setName("HelloJPA");
+			Member member = em.find(Member.class, 150L);
+			member.setName("AAAAA");
 
-			List<Member> result = em.createQuery("select m from Member as m", Member.class)
-				.setFirstResult(5)
-				.setMaxResults(8)
-				.getResultList();
-
-			for (Member member : result) {
-				System.out.println("member.getName() = " + member.getName());
-			}
+			em.detach(member);
 
 			tx.commit();
 		} catch (Exception e) {
